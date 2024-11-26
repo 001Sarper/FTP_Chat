c2e9404aab0262062942f472414e1e98cdb475e6
001Sarper <sarpereroglu2@gmail.com>
2024-11-26 20:18:34 +0100

    Main Structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter
 from customtkinter import *
 from PIL import Image
+import json
 
 
 def CenterWindowToDisplay(Screen: CTk, width: int, height: int, scale_factor: float = 1.0):
@@ -12,11 +13,37 @@ def CenterWindowToDisplay(Screen: CTk, width: int, height: int, scale_factor: fl
     return f"{width}x{height}+{x}+{y}"
 
 class ToplevelWindow(customtkinter.CTkToplevel):
+    def save_changes(self):
+        with open("config.json", "r") as json_file:
+            config = json_file.read()
+        with open("config.json", "w") as json_file:
+            new_config = json.loads(config)
+            new_config['chat_username'] = self.chat_username.get("0.0", "end").strip()
+            new_config['chatlog_path'] = self.chatlog_path.get("0.0", "end").strip()
+            new_config['ftp_username'] = self.ftp_username.get("0.0", "end").strip()
+            new_config['ftp_password'] = self.ftp_password.get("0.0", "end").strip()
+            new_config['ftp_server'] = self.ftp_server.get("0.0", "end").strip()
+            new_config_json = json.dumps(new_config, indent=2)
+            json_file.write(new_config_json)
+            print("Config Saved Successfully")
+            self.destroy()
+
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.geometry(CenterWindowToDisplay(self, 500, 450, self._get_window_scaling()))
         self.resizable(False, False)
         self.title("FTP Settings")
+
+        with open("config.json", "r") as json_file:
+            config1 = json_file.read()
+            config2 = json.loads(config1)
+            chat_username = config2['chat_username']
+            chatlog_path = config2['chatlog_path']
+            ftp_username = config2['ftp_username']
+            ftp_password = config2['ftp_password']
+            ftp_server = config2['ftp_server']
+
 
         self.title1 = CTkLabel(self, text="FTP Settings", anchor="n", font=CTkFont(size=14))
         self.title1.place(rely=0.02, relx=0.41)
@@ -26,6 +53,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
 
         self.chat_username = CTkTextbox(master=self, corner_radius=8, height=25, width=450,  border_color="#FFCC70", border_width=2, scrollbar_button_color="#FFCC70")
         self.chat_username.place(relx=0.05, rely=0.14)
+        self.chat_username.insert("0.0", chat_username)
 
         self.chatlog_path_label = customtkinter.CTkLabel(self, text="Chatlog Path", anchor="nw")
         self.chatlog_path_label.place(rely=0.26, relx=0.05)
@@ -33,6 +61,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.chatlog_path = CTkTextbox(master=self, corner_radius=8, height=25, width=450, border_color="#FFCC70",
                                         border_width=2, scrollbar_button_color="#FFCC70")
         self.chatlog_path.place(relx=0.05, rely=0.30)
+        self.chatlog_path.insert("0.0", chatlog_path)
 
         self.ftp_username_label = customtkinter.CTkLabel(self, text="FTP Username", anchor="nw")
         self.ftp_username_label.place(rely=0.42, relx=0.05)
@@ -40,6 +69,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.ftp_username = CTkTextbox(master=self, corner_radius=8, height=25, width=450, border_color="#FFCC70",
                                         border_width=2, scrollbar_button_color="#FFCC70")
         self.ftp_username.place(relx=0.05, rely=0.46)
+        self.ftp_username.insert("0.0", ftp_username)
 
         self.ftp_password_label = customtkinter.CTkLabel(self, text="FTP Password", anchor="nw")
         self.ftp_password_label.place(rely=0.58, relx=0.05)
@@ -47,6 +77,7 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.ftp_password = CTkTextbox(master=self, corner_radius=8, height=25, width=450, border_color="#FFCC70",
                                        border_width=2, scrollbar_button_color="#FFCC70")
         self.ftp_password.place(relx=0.05, rely=0.62)
+        self.ftp_password.insert("0.0", ftp_password)
 
         self.ftp_port_label = customtkinter.CTkLabel(self, text="FTP Server IP", anchor="nw")
         self.ftp_port_label.place(rely=0.74, relx=0.05)
@@ -54,15 +85,15 @@ class ToplevelWindow(customtkinter.CTkToplevel):
         self.ftp_server = CTkTextbox(master=self, corner_radius=8, height=25, width=150, border_color="#FFCC70",
                                        border_width=2, scrollbar_button_color="#FFCC70")
         self.ftp_server.place(relx=0.05, rely=0.78)
+        self.ftp_server.insert("0.0", ftp_server)
 
         save_img = Image.open("icons/save_icon.png")
 
         self.save_changes_btn = CTkButton(master=self, text="Save Changes", width=50, height=30, corner_radius=16, fg_color="#e63505",
-                             hover_color="#b52a04", border_color="#e63505", border_width=2, image=CTkImage(dark_image=save_img, light_image=save_img))
+                             hover_color="#b52a04", border_color="#e63505", border_width=2, image=CTkImage(dark_image=save_img, light_image=save_img), command=self.save_changes)
         self.save_changes_btn.place(relx=0.5, rely=0.85, anchor="sw")
 
 
-    def save_changes_btn(self):
 
 
 class App(customtkinter.CTk):
